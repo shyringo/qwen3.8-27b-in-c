0fd83fa03270db25ba87b63007174a65106711e0
shy <shyringo@163.com>
2026-08-28 02:54:00 +0800

    Support the local API on macOS

--- src/cli/qwen38_main.c
+++ src/cli/qwen38_main.c
@@ -977,7 +977,11 @@ static int q38_server(Q38Model *model, Q38Tokenizer *tokenizer,
     struct sockaddr_in address;
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (inet_pton(AF_INET, "127.0.0.1", &address.sin_addr) != 1) {
+        fprintf(stderr, "qwen38: unable to create loopback address\n");
+        close(server);
+        return 0;
+    }
     address.sin_port = htons((uint16_t)options->server_port);
     if (bind(server, (const struct sockaddr *)&address, sizeof(address)) != 0 ||
         listen(server, 16) != 0) {
